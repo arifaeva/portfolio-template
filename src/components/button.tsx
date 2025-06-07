abc292fdb397362b779784b25ead4c683f258f7d
arifaeva <arifa.eva.ixa@gmail.com>
2025-06-07 21:28:02 +0700

    style: change secondary button color in dark mode

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ const style = tv({
       primary:
         "bg-black text-white hover:bg-zinc-800 active:bg-zinc-800 dark:hover:bg-black/60 active:bg-black-60",
       secondary:
-        "bg-transparent border border-neutral-500 text-neutral-500 hover:bg-zinc-100 active:bg-zinc-100",
+        "bg-transparent border border-neutral-500 text-neutral-500 dark:hover:text-neutral-600 dark:text-neutral-100 dark:border-neutral-100 hover:bg-zinc-100 active:bg-zinc-100",
     },
     size: {
       sm: "px-3 py-2 text-sm",
