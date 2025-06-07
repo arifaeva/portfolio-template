048289dac857256d6af5ceb1712e0d8e654c6f97
arifaeva <arifa.eva.ixa@gmail.com>
2025-06-07 21:30:06 +0700

    style: complete text in footer

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="flex justify-center items-center bg-zinc-50 dark:bg-zinc-800">
       <div className="max-w-[1080px] w-full border border-t border-l-0 border-r-0 border-b-0 dark:border-neutral-500 lg:px-16 lg:py-8 py-3">
         <p className="text-center text-gray-500 dark:text-white">
-          <span className="font-semibold">&copy;Devscale.</span> Build with
+          <span className="font-semibold">&copy;Devscale.</span> Build with ❤
         </p>
       </div>
     </footer>
