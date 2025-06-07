909a35705ecabba7cfb230d2fafee655ba548307
arifaeva <arifa.eva.ixa@gmail.com>
2025-06-07 21:29:14 +0700

    style: fix color on burger menu icon

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -66,15 +66,10 @@ export const Header = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   width="32"
                   height="32"
-                  viewBox="0 0 14 14"
-                  className="h-[20px] w-[20px] fill-gray-600 dark:fill-white"
+                  viewBox="0 0 24 24"
+                  className="lg:h-[30px] lg:w-[30px] w-[26px] h-[26px] fill-gray-600 dark:fill-white"
                 >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 2H.5m13 5H.5m13 5H.5"
-                  />
+                  <path d="M4 18q-.425 0-.712-.288T3 17t.288-.712T4 16h16q.425 0 .713.288T21 17t-.288.713T20 18zm0-5q-.425 0-.712-.288T3 12t.288-.712T4 11h16q.425 0 .713.288T21 12t-.288.713T20 13zm0-5q-.425 0-.712-.288T3 7t.288-.712T4 6h16q.425 0 .713.288T21 7t-.288.713T20 8z" />
                 </svg>
               )}
             </button>
